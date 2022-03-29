fdd7157275b9c2c280f94bad826823a483e3d273
Randy Duran <randyduran@Randys-MacBook-Air.local>
2022-03-29 21:08:19 +0200

    update

--- src/AddDentist.js
+++ src/AddDentist.js
@@ -1,70 +1,75 @@
 import React, {useState} from "react";
 
 
-
 const AddDentist = ({addContact}) => {
- const [dentist, setDentist] = useState({
-         first_name: "",
-         last_name: "",
-         phone: "",
-         email: "",
-         isSick: false,
-         id: Date.now()
- })
-
- const onChangeDentist = (e) =>{
-         setDentist({...dentist, [e.target.name] : e.target.value})
- }
-
- const handleSubmit = (e) =>{
-         e.preventDefault();
-         addContact(dentist);
-         setDentist({first_name: "",last_name: "",number: "",email: ""});
- }
- 
-
-return (
-<div>
-        
-        <form onSubmit={handleSubmit}>
-
-        <input 
-        type="text"
-        required
-        value = {dentist.first_name}
-        placeholder="name"
-        name = "first_name"
-        onChange={onChangeDentist}/>
-        
-        <input 
-        type="text"
-        required
-        value = {dentist.last_name}
-        name="last_name"
-        placeholder="surname"
-        onChange={onChangeDentist}/>
+    const [dentist, setDentist] = useState({
+            first_name: "",
+            last_name: "",
+            phone: "",
+            email: "",
+            isSick: false,
+            id: Date.now()
+    })
+   
+    const onChangeDentist = (e) =>{
+            setDentist({...dentist, [e.target.name] : e.target.value})
+    }
+   
+    const handleSubmit = (e) =>{
+            e.preventDefault();
+            addContact(dentist);
+            setDentist({first_name: "",last_name: "",number: "",email: ""});
+    }
     
-        <input 
-        type="email"
-        required
-        value = {dentist.email}
-        name="email"
-        placeholder="email"
-        onChange={onChangeDentist}/>
+    const handleSick = (e) => {
+            e.preventDefault();
+            const isSick = dentist.isSick;
+        {sick && <p style={{color:"red"}}> </p>}}
 
-        <input 
-        type="number"
-        required
-        value = {dentist.number}
-        name="phone"
-        placeholder="phone"
-        onChange={onChangeDentist}/>
-        
-        <button type="submit">Add</button>
-        </form>
-        
-</div>
-)}
-
-
-export default AddDentist
+   
+   return (
+   <div>
+           
+           <form onSubmit={handleSubmit}>
+   
+           <input 
+           type="text"
+           required
+           value = {dentist.first_name}
+           placeholder="name"
+           name = "first_name"
+           onChange={onChangeDentist}/>
+           
+           <input 
+           type="text"
+           required
+           value = {dentist.last_name}
+           name="last_name"
+           placeholder="surname"
+           onChange={onChangeDentist}/>
+       
+           <input 
+           type="email"
+           required
+           value = {dentist.email}
+           name="email"
+           placeholder="email"
+           onChange={onChangeDentist}/>
+   
+           <input 
+           type="number"
+           required
+           value = {dentist.number}
+           name="phone"
+           placeholder="phone"
+           onChange={onChangeDentist}/>
+           
+           <button type="submit">Add Dentist</button>
+           <button handleSick={isSick}>Sick!</button>
+           </form>
+           
+   </div>
+   )}
+   
+   
+   export default AddDentist
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { useState }from "react";
-import AddDentist from './AddDentist'
+import AddDentist from './AddAssistant'
+import AddAssistant from "./AddAssistant";
+import AddClient from "./AddClient";
 import UserList from "./UserList";
 
 export default function Home() {
@@ -12,6 +14,8 @@ export default function Home() {
 
     return(
     <div>
+        <AddClient addContact={addContact}/>
+        <AddAssistant addContact={addContact}/>
         <AddDentist addContact={addContact}/>
         <UserList contacts={contacts}/>
     </div>
